8d3e5af5480ec8a2eec66da0172ec02c8344fce4
yuliang <yuliang@MIT.edu>
2019-02-08 17:41:59 -0500

    Added code for batch predictions

--- astronet/astronet/predict.py
+++ astronet/astronet/predict.py
@@ -20,10 +20,12 @@ from __future__ import print_function
 
 import argparse
 import sys
+import os
 
 import matplotlib.pyplot as plt
 import numpy as np
 import tensorflow as tf
+import pandas as pd
 
 from astronet import models
 from astronet.data import preprocess
@@ -40,13 +42,13 @@ parser.add_argument(
     "--config_name",
     type=str,
     help="Name of the model and training configuration. Exactly one of "
-    "--config_name or --config_json is required.")
+         "--config_name or --config_json is required.")
 
 parser.add_argument(
     "--config_json",
     type=str,
     help="JSON string or JSON file containing the model and training "
-    "configuration. Exactly one of --config_name or --config_json is required.")
+         "configuration. Exactly one of --config_name or --config_json is required.")
 
 parser.add_argument(
     "--model_dir",
@@ -55,122 +57,129 @@ parser.add_argument(
     help="Directory containing a model checkpoint.")
 
 parser.add_argument(
-    "--kepler_data_dir",
+    "--tess_data_dir",
     type=str,
     required=True,
     help="Base folder containing Kepler data.")
 
 parser.add_argument(
-    "--kepler_id",
-    type=int,
-    required=True,
-    help="Kepler ID of the target star.")
-
-parser.add_argument(
-    "--period", type=float, required=True, help="Period of the TCE, in days.")
-
-parser.add_argument("--t0", type=float, required=True, help="Epoch of the TCE.")
-
-parser.add_argument(
-    "--duration",
-    type=float,
-    required=True,
-    help="Duration of the TCE, in days.")
-
-parser.add_argument(
-    "--output_image_file",
+    "--input_tce_csv_file",
     type=str,
-    help="If specified, path to an output image file containing feature plots. "
-    "Must end in a valid image extension, e.g. png.")
+    required=True,
+    help="CSV file containing the TESS TCE table. Must contain "
+         "columns: row_id, tic_id, toi_id, Period, Duration, "
+         "Epoc (t0).")
+
+parser.add_argument(
+    "--output_image_dir",
+    type=str,
+    help="If specified, output image files containing feature plots will be saved to this directory. ")
+
+parser.add_argument(
+    "--output_file",
+    type=str,
+    default='predictions.txt',
+    help="Name of file in which predictions will be saved.")
 
 
-def _process_tce(feature_config):
-  """Reads and process the input features of a Threshold Crossing Event.
+def _process_tce(feature_config, tce):
+    """Reads and process the input features of a Threshold Crossing Event.
 
-  Args:
-    feature_config: ConfigDict containing the feature configurations.
+    Args:
+      tce: row from TCE CSV file, read in as a Pandas dataframe.
+      feature_config: ConfigDict containing the feature configurations.
 
-  Returns:
-    A dictionary of processed light curve features.
+    Returns:
+      A dictionary of processed light curve features.
 
-  Raises:
-    ValueError: If feature_config contains features other than 'global_view'
-    and 'local_view'.
-  """
-  if not {"global_view", "local_view"}.issuperset(feature_config.keys()):
-    raise ValueError(
-        "Only 'global_view' and 'local_view' features are supported.")
+    Raises:
+      ValueError: If feature_config contains features other than 'global_view'
+      and 'local_view'.
+    """
+    if not {"global_view", "local_view"}.issuperset(feature_config.keys()):
+        raise ValueError(
+            "Only 'global_view' and 'local_view' features are supported.")
 
-  # Read and process the light curve.
-  time, flux = preprocess.read_and_process_light_curve(FLAGS.kepler_id,
-                                                       FLAGS.kepler_data_dir)
-  time, flux = preprocess.phase_fold_and_sort_light_curve(
-      time, flux, FLAGS.period, FLAGS.t0)
+    # Read and process the light curve.
+    time, flux = preprocess.read_and_process_light_curve(tce.tic_id, FLAGS.tess_data_dir, sector=tce.Sectors,
+                                                         cam=tce.camera,
+                                                         ccd=tce.ccd)
+    time, flux = preprocess.phase_fold_and_sort_light_curve(
+        time, flux, tce.Period, tce.Epoc)
 
-  # Generate the local and global views.
-  features = {}
+    # Generate the local and global views.
+    features = {}
 
-  if "global_view" in feature_config:
-    global_view = preprocess.global_view(time, flux, FLAGS.period)
-    # Add a batch dimension.
-    features["global_view"] = np.expand_dims(global_view, 0)
+    if "global_view" in feature_config:
+        global_view = preprocess.global_view(time, flux, tce.Period)
+        # Add a batch dimension.
+        features["global_view"] = np.expand_dims(global_view, 0)
 
-  if "local_view" in feature_config:
-    local_view = preprocess.local_view(time, flux, FLAGS.period, FLAGS.duration)
-    # Add a batch dimension.
-    features["local_view"] = np.expand_dims(local_view, 0)
+    if "local_view" in feature_config:
+        local_view = preprocess.local_view(time, flux, tce.Period, tce.Duration)
+        # Add a batch dimension.
+        features["local_view"] = np.expand_dims(local_view, 0)
 
-  # Possibly save plots.
-  if FLAGS.output_image_file:
-    ncols = len(features)
-    fig, axes = plt.subplots(1, ncols, figsize=(10 * ncols, 5), squeeze=False)
+    # Possibly save plots.
+    if FLAGS.output_image_dir:
+        ncols = len(features)
+        fig, axes = plt.subplots(1, ncols, figsize=(10 * ncols, 5), squeeze=False)
 
-    for i, name in enumerate(sorted(features)):
-      ax = axes[0][i]
-      ax.plot(features[name][0], ".")
-      ax.set_title(name)
-      ax.set_xlabel("Bucketized Time (days)")
-      ax.set_ylabel("Normalized Flux")
+        for i, name in enumerate(sorted(features)):
+            ax = axes[0][i]
+            ax.plot(features[name][0], ".")
+            ax.set_title(name)
+            ax.set_xlabel("Bucketized Time (days)")
+            ax.set_ylabel("Normalized Flux")
 
-    fig.tight_layout()
-    fig.savefig(FLAGS.output_image_file, bbox_inches="tight")
+        fig.tight_layout()
+        fig.savefig(os.path.join(FLAGS.output_image_dir, str(tce.tic_id) + '.png'), bbox_inches="tight")
 
-  return features
+    return features
 
 
 def main(_):
-  model_class = models.get_model_class(FLAGS.model)
+    model_class = models.get_model_class(FLAGS.model)
 
-  # Look up the model configuration.
-  assert (FLAGS.config_name is None) != (FLAGS.config_json is None), (
-      "Exactly one of --config_name or --config_json is required.")
-  config = (
-      models.get_model_config(FLAGS.model, FLAGS.config_name)
-      if FLAGS.config_name else config_util.parse_json(FLAGS.config_json))
-  config = configdict.ConfigDict(config)
+    # Look up the model configuration.
+    assert (FLAGS.config_name is None) != (FLAGS.config_json is None), (
+        "Exactly one of --config_name or --config_json is required.")
+    config = (
+        models.get_model_config(FLAGS.model, FLAGS.config_name)
+        if FLAGS.config_name else config_util.parse_json(FLAGS.config_json))
+    config = configdict.ConfigDict(config)
 
-  # Create the estimator.
-  estimator = estimator_util.create_estimator(
-      model_class, config.hparams, model_dir=FLAGS.model_dir)
+    # Create the estimator.
+    estimator = estimator_util.create_estimator(
+        model_class, config.hparams, model_dir=FLAGS.model_dir)
 
-  # Read and process the input features.
-  features = _process_tce(config.inputs.features)
+    # Read and process the input features.
+    tce_table = pd.read_csv(FLAGS.input_tce_csv_file, header=0, usecols=[0, 1, 2, 3, 7, 8, 9, 10, 11, 12, 13, 18],
+                            dtype={'Sectors':
+                                       int,
+                                   'camera': int,
+                                   'ccd': int})
 
-  # Create an input function.
-  def input_fn():
-    return {
-        "time_series_features":
-            tf.estimator.inputs.numpy_input_fn(
-                features, batch_size=1, shuffle=False, queue_capacity=1)()
-    }
+    for ind, tce in tce_table.iterrows():
+        features = _process_tce(config.inputs.features, tce)
 
-  # Generate the predictions.
-  for predictions in estimator.predict(input_fn):
-    assert len(predictions) == 1
-    print("Prediction:", predictions[0])
+        # Create an input function.
+        def input_fn():
+            return {
+                "time_series_features":
+                    tf.estimator.inputs.numpy_input_fn(
+                        features, batch_size=1, shuffle=False, queue_capacity=1)()
+            }
+
+        # Generate the predictions.
+        for predictions in estimator.predict(input_fn):
+            assert len(predictions) == 1
+            print(tce.tic_id, "Prediction:", predictions[0])
+
+            print(str(tce.tic_id)+' '+str(predictions[0]), file=open(FLAGS.output_file, 'a'))
 
 
 if __name__ == "__main__":
-  tf.logging.set_verbosity(tf.logging.INFO)
-  FLAGS, unparsed = parser.parse_known_args()
-  tf.app.run(main=main, argv=[sys.argv[0]] + unparsed)
+    tf.logging.set_verbosity(tf.logging.INFO)
+    FLAGS, unparsed = parser.parse_known_args()
+    tf.app.run(main=main, argv=[sys.argv[0]] + unparsed)
